c3e95d2c76e660e15cc41d03939d20c3a85465b4
dwieb <dwayne.harmon@gmail.com>
2021-02-03 12:44:06 -0600

    Added User model tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,39 @@
 require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe User, type: :model do
 
-
-describe User do 
   #! User can save first_name
-  it "do something here" do
-    
+  it "has a first name" do
+    user = User.create!(
+      last_name: "Anthony",
+      email: "dwayne@email.com",
+      password: "password"
+    )
+    expect(user).to_not be_valid
+
+    user.last_name = "Dwayne"
+    expect(user).to be_valid
   end
+
+
+  it "has a last name" do
+    user = User.create!(
+      first_name: "Dwayne",
+      email: "dwayne@email.com",
+      password: "password"
+    )
+    expect(user).to_not be_valid
+
+    user.first_name = "Dwayne"
+    expect(user).to be_valid
+  end
+
+  # it "Ensures presence of first name" do
+  #   user = User.new(email: "dwayne@email.com", last_name: "Anthony", ).save
+  #   expect(user).to eq(false)
+  # end  
+  # it "Ensures presence of last name" do
+  #   user = User.new(email: "dwayne@email.com", first_name: "Dwayne", ).save
+  #   expect(user).to eq(false)
+  # end
 end
